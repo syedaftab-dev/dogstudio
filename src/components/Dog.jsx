de628b29aea8e75ee57a2eb8f24551042a7e2828
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2026-01-17 00:14:31 +0530

    added Dog model and postioned it

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,23 +1,39 @@
 import React from 'react'
 import { Canvas, useThree } from '@react-three/fiber' // this will contain the 3js file renderer created
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF } from '@react-three/drei'
 
 
 export const Dog = () => {
 
-    useThree(({camera,scene,gl})=>{
-      console.log(camera.position)
-    })
+  // this will load the model
+  const model = useGLTF("/models/dog.drc.glb") // path is relative to the public folder
 
+  useThree(({camera,scene,gl})=>{
+    console.log(camera.position);
+    // the model was to far from camera so we moved it closer
+    camera.position.z = 0.55;
+  })
   return (
     <>
-        {/* ithne se code se humne camera,light aur ek cube baneye with the help of rect-three/fiber */}
-        <mesh>
-            <meshBasicMaterial color={0x00FF00}/>
-            <boxGeometry args={[1,1,1]}/>
-        </mesh>
-        <OrbitControls />  {/* this will allow us to rotate the camera around the object */}
+        {/* will display the model */}
+        <primitive object={model.scene} position={[0.25,-0.55,0]} rotation={[0,Math.PI/3.9,0]}/> 
+        <directionalLight intensity={10} position={[0,0,5]} color={0xFFFFFF}/>
+        <OrbitControls />
     </>
     )
 }
 
+// {/* ithne se code se humne camera,light aur ek cube baneye with the help of rect-three/fiber */}
+//         <mesh>
+//             <meshBasicMaterial color={0x00FF00}/>
+//             <boxGeometry args={[1,1,1]}/>
+//         </mesh>
+//         <OrbitControls />  {/* this will allow us to rotate the camera around the object */}
+
+
+/*
+
+Materials which can be seen without light
+1. MeshBasicMaterial
+
+*/
