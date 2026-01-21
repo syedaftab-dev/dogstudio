bdcfba8b9d9a8834e4ee0d5e0b8c1b4aad3872ee
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2026-01-21 22:51:36 +0530

    fragment and vertex shader function

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -36,16 +36,51 @@ export const Dog = () => {
   },[actions])
 
   // normal map and matcap apply on model
-  const [normalMap, sampleMatCap, branchMap, branchNormalMap ] = useTexture(["/dog_normals.jpg", "/matcap/mat-2.png","/branches_diffuse.jpg","/branches_normals.jpg"]).map((texture)=>{
+  const [normalMap,branchMap, branchNormalMap ] = useTexture(["/dog_normals.jpg","/branches_diffuse.jpg","/branches_normals.jpg"]).map((texture)=>{
     texture.flipY = false;
     texture.colorSpace = THREE.SRGBColorSpace;
     return texture;
   });
   
+  // loading matcap materials
+  const [mat1,mat2,mat3,mat4,mat5,mat6,mat7,mat8,mat9,mat10,mat11,mat12,mat13,mat14,mat15,mat16,mat17,mat18,mat19,mat20] = (useTexture([
+    "./matcap/mat-1.png",
+    "./matcap/mat-2.png",
+    "./matcap/mat-3.png",
+    "./matcap/mat-4.png",
+    "./matcap/mat-5.png",
+    "./matcap/mat-6.png",
+    "./matcap/mat-7.png",
+    "./matcap/mat-8.png",
+    "./matcap/mat-9.png",
+    "./matcap/mat-10.png",
+    "./matcap/mat-11.png",
+    "./matcap/mat-12.png",
+    "./matcap/mat-13.png",
+    "./matcap/mat-14.png",
+    "./matcap/mat-15.png",
+    "./matcap/mat-16.png",
+    "./matcap/mat-17.png",
+    "./matcap/mat-18.png",
+    "./matcap/mat-19.png",
+    "./matcap/mat-20.png",
+  ])).map(texture => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+    return texture;
+  })
+
+
+  // for a short time of transition on material we can see on model we have 2 material
+  const material = useRef({
+    uMatcap1: {value: mat1},
+    uMatcap2: {value: mat19},
+    uProgress: {value: 0.5} // percentage to share on model 50% of 0.5 50%-mat1 and 50%-mat2
+  })
+
   // creating materials for dog
   const dogMaterial = new THREE.MeshMatcapMaterial({
     normalMap: normalMap,
-    matcap: sampleMatCap
+    matcap: mat2
   });
   
   // creating materials for branches
@@ -53,6 +88,44 @@ export const Dog = () => {
     normalMap: branchNormalMap,
     map: branchMap
   });
+
+  // applying shader when hover
+  // function to edit vertex , fragment shader before it render on every frame of objectslll
+  function onBeforeCompile(shader){
+
+     shader.uniforms.uMatcapTexture1 = material.current.uMatcap1
+        shader.uniforms.uMatcapTexture2 = material.current.uMatcap2
+        shader.uniforms.uProgress = material.current.uProgress
+
+        // Store reference to shader uniforms for GSAP animation
+
+        shader.fragmentShader = shader.fragmentShader.replace(
+            "void main() {",
+            `
+        uniform sampler2D uMatcapTexture1;
+        uniform sampler2D uMatcapTexture2;
+        uniform float uProgress;
+
+        void main() {
+        `
+        )
+
+        shader.fragmentShader = shader.fragmentShader.replace(
+            "vec4 matcapColor = texture2D( matcap, uv );",
+            `
+          vec4 matcapColor1 = texture2D( uMatcapTexture1, uv );
+          vec4 matcapColor2 = texture2D( uMatcapTexture2, uv );
+          float transitionFactor  = 0.2;
+          
+          float progress = smoothstep(uProgress - transitionFactor,uProgress, (vViewPosition.x+vViewPosition.y)*0.5 + 0.5);
+
+          vec4 matcapColor = mix(matcapColor2, matcapColor1, progress );
+        `
+        )
+    }
+
+    dogMaterial.onBeforeCompile = onBeforeCompile;
+
 
   // hamare pass dog wale model main 108 objects,aur traverse har object ke liye ek baar chalega
   model.scene.traverse((child) => {
